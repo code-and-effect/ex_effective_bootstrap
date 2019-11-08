10d2922bbc7181c1312412258404e351bda8576d
Matt Riemer <matthew@codeandeffect.com>
2019-11-08 08:05:10 -0700

    error classes

--- lib/ex_effective_bootstrap/form.ex
+++ lib/ex_effective_bootstrap/form.ex
@@ -25,21 +25,21 @@ defmodule ExEffectiveBootstrap.Form do
   end
 
   defp merge_options(form_data, _action, options \\ []) do
-    options =
-      options
-      |> merge_class(@class)
-      |> merge_other(@other)
-      |> merge_error(form_data)
+    options
+    |> merge_class(@class)
+    |> merge_other(@other)
+    |> merge_error(form_data)
   end
 
   defp merge_class(options, classes) do
-    Keyword.update(options, :class, Enum.join(classes, " "), fn class -> Enum.join([class] ++ classes, " ") end)
+    Keyword.update(options, :class, Enum.join(classes, " "), fn class ->
+      Enum.join([class] ++ classes, " ")
+    end)
   end
 
   defp merge_other(options, opts), do: Keyword.merge(options, opts)
 
   defp merge_error(options, %{changes: changes}) when map_size(changes) == 0, do: options
-  defp merge_error(options, %Changeset{valid?: false}), do: merge_class(options, @error)
+  defp merge_error(options, %{valid?: false}), do: merge_class(options, @error)
   defp merge_error(options, _), do: options
-
 end
