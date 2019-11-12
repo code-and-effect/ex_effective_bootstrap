73e5c8d3a5d7f7d19b0ed6888acfdf430b985bf8
Matt Riemer <matthew@codeandeffect.com>
2019-11-11 20:27:56 -0700

    merging options

--- lib/ex_effective_bootstrap/inputs.ex
+++ lib/ex_effective_bootstrap/inputs.ex
@@ -2,6 +2,8 @@ defmodule ExEffectiveBootstrap.Inputs do
   use Phoenix.HTML
   alias Phoenix.HTML.Form, as: PhxForm
 
+  @valid_label "Look's good!"
+  @invalid_label "is invalid"
 
 # <div class="form-group">
 #   <label for="user_name">Name</label>
@@ -14,10 +16,10 @@ defmodule ExEffectiveBootstrap.Inputs do
     opts = input_options(form, field, options)
 
     content_tag :div, opts.wrapper do
-      label = PhxForm.label(form, field, opts.label)
-      input = apply(PhxForm, opts.type, [form, field, opts.input])
-      valid = feedback_valid(form, field, opts.feedback_valid)
-      invalid = feedback_invalid(form, field, opts.feedback_invalid)
+      label = label_tag(form, field, opts.label)
+      input = input_tag(form, field, opts.type, opts.input)
+      valid = valid_tag(form, field, opts.valid)
+      invalid = invalid_tag(form, field, opts.invalid)
 
       [label, input, valid, invalid]
     end
@@ -25,23 +27,51 @@ defmodule ExEffectiveBootstrap.Inputs do
 
   defp input_options(form, field, options) do
     %{
+      label: options[:label],
       type: (options[:as] || options[:type] || PhxForm.input_type(form, field)),
-      input: [class: "form-control", required: true],
-      label: humanize(field),
-      wrapper: [class: "form-group"],
-      feedback_valid: "Look's good!",
-      feedback_invalid: "can't be blank",
+      valid: options[:valid_feedback],
+      invalid: options[:invalid_feedback],
+      wrapper: merge_options(options[:wrapper], [class: "form-group"]),
+      input: input_field_options(form, field, options)
     }
   end
 
-  defp feedback_valid(form, _, false), do: []
-  defp feedback_valid(form, _, label) do
-    content_tag(:div, label, [class: "valid-feedback"])
+  defp input_field_options(form, field, options) do
+    default = [class: "form-control"]
+    phoenix = PhxForm.input_validations(form, field)
+    options = Keyword.drop(options, [:label, :as, :type, :valid_feedback, :invalid_feedback, :wrapper])
+
+    default
+    |> merge_options(phoenix)
+    |> merge_options(options)
   end
 
-  defp feedback_invalid(form, _, false), do: []
-  defp feedback_invalid(form, _, label) do
-    content_tag(:div, label, [class: "invalid-feedback"])
+  defp label_tag(_form, _field, false), do: []
+  defp label_tag(form, field, label), do: PhxForm.label(form, field, label || humanize(field))
+
+  defp input_tag(form, field, type, options) do
+    apply(PhxForm, type, [form, field, options])
   end
+
+  defp valid_tag(_form, _field, false), do: []
+  defp valid_tag(_form, _field, label) do
+    content_tag(:div, label || @valid_label, [class: "valid-feedback"])
+  end
+
+  defp invalid_tag(_form, _field, false), do: []
+  defp invalid_tag(_form, _field, label) do
+    content_tag(:div, label || @invalid_label, [class: "invalid-feedback"])
+  end
+
+  defp merge_options(nil, opts) when is_list(opts), do: opts
+  defp merge_options(options, nil) when is_list(options), do: options
+  defp merge_options(options, opts) when is_list(options) and is_list(opts) do
+    Keyword.merge(options, opts) |> merge_class(options[:class], opts[:class])
+  end
+
+  defp merge_class(options, nil, nil), do: options
+  defp merge_class(options, class, nil), do: Keyword.merge(options, [class: class])
+  defp merge_class(options, nil, class), do: Keyword.merge(options, [class: class])
+  defp merge_class(options, a, b), do: Keyword.merge(options, [class: "#{a} #{b}"])
 
 end
