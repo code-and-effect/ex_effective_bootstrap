1e7da545031d331c43488e38fa9958bb4b0219e9
Matt Riemer <matthew@codeandeffect.com>
2019-11-15 10:01:52 -0700

    form JS done

--- assets/js/form.js
+++ assets/js/form.js
@@ -13,7 +13,12 @@ export default class EffectiveForm {
     let valid = form.checkValidity();
     let $form = $(form);
 
-    this.reset($form);
+    if ($form.hasClass('.was-validated')) {
+      this.reset($form);
+    } else {
+      this.resetServerSideValidations($form)
+    }
+
     if (valid) { this.submitting($form); } else { this.invalidate($form); }
 
     return valid;
@@ -48,23 +53,29 @@ export default class EffectiveForm {
   }
 
   reset($form) {
-    if($form.is('form') == false) { $form = $form.closest('form') };
+    this.resetClientSideValidations($form)
+    this.resetServerSideValidations($form)
+  }
 
+  resetClientSideValidations($form) {
     // Reset the form
     $form.removeClass('was-validated');
     $form.removeClass('form-is-invalid');
     $form.removeClass('form-is-valid');
 
-    // Reset any individual input server side validations
-    $form.find('.alert.is-invalid').remove();
-    $form.find('.is-invalid').removeClass('is-invalid');
-    $form.find('.is-valid').removeClass('is-valid');
-
     // Reset the submit button
     $form.find('.effective-current-submit').removeClass('.effective-current-submit');
     $form.find('[type=submit]').removeAttr('disabled');
 
+    this.resetClientSideValidations($form)
+
     return true;
+  }
+
+  resetServerSideValidations($form) {
+    $form.find('.alert.is-invalid').remove();
+    $form.find('.is-invalid').removeClass('is-invalid');
+    $form.find('.is-valid').removeClass('is-valid');
   }
 
   flashSuccess() {
@@ -86,43 +97,4 @@ export default class EffectiveForm {
         if(fun) { fun(); }
       });
   }
-
-
 }
-
-
-// flash: ($form, status, message, skip_success = false) ->
-//     return unless @current_submit.length > 0
-
-// if status == 'danger' || status == 'error'
-//       @current_submit.find('.eb-icon-x').show().delay(1000).fadeOut('slow', -> $form.trigger('effective-form:error-animation-done', @remote_form_commit, message))
-//     else
-// @current_submit.find('.eb-icon-check').show().delay(1000).fadeOut('slow', -> $form.trigger('effective-form:success-animation-done', @remote_form_commit, message))
-
-// if message ? && !(status == 'success' && skip_success)
-//       @current_submit.prepend(@buildFlash(status, message))
-
-
-
-
-// console.log('here')
-
-// Sets EffectiveBootstrap.current_click.
-// This displays the spinner here, and directs any flash messages before and after loadRemoteForm
-// $(document).on('click', '.effective-form-actions button[type=submit],.effective-form-actions a[data-remote]', function(event) {
-//   EffectiveForm.setCurrentSubmit($(event.currentTarget));
-//   EffectiveForm.spin();
-// });
-
-// // Clear
-// $(document).on('reset', 'form', function(event) {
-//   $form = $(event.currentTarget);
-//   EffectiveForm.reset($form);
-// });
-
-// $(document).on('clear', 'form', function(event) {
-//   $form = $(event.currentTarget);
-//   EffectiveForm.reset($form);
-//   setTimeout(function () { EffectiveForm.clear($form); });
-// });
-
