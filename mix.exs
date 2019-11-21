b627fa50226b13ca48cfe86091a19d708252db10
Matt Riemer <matthew@codeandeffect.com>
2019-11-20 20:43:04 -0700

    dev deps

--- mix.exs
+++ mix.exs
@@ -33,7 +33,10 @@ defmodule ExEffectiveBootstrap.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ecto, "~> 2.2 or ~> 3.0"}
+      {:ecto, "~> 2.2 or ~> 3.0"},
+      {:ex_check, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
