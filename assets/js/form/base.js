dfc00c380de0c700ea457d47458a6c239580a5fc
Matt Riemer <matthew@codeandeffect.com>
2019-12-08 15:02:36 -0700

    bump for new version

--- assets/js/form/base.js
+++ assets/js/form/base.js
@@ -11,7 +11,7 @@ export default class EffectiveForm {
     this.currentSubmit = "";
   }
 
-  good() { return "you good"; }
+  good() { return "you good 0.1.10"; }
 
   initialize(target) {
     $(target || document).find('[data-input-js-options]:not(.initialized)').each(function (i, element) {
